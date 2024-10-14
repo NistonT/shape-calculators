0bf371dae572d74e520f9eb870bf1269bafcdfea
NistonN <nisprogrammer@gmail.com>
2024-10-15 00:52:09 +0300

    commit

--- src/containers/Main/Navigator/Navigator.tsx
+++ src/containers/Main/Navigator/Navigator.tsx
@@ -4,7 +4,7 @@ export const Navigator = ({ id_nav, name, to, img }: any) => {
 	return (
 		<>
 			<NavLink key={id_nav} to={to} className='xl:w-1/4 md:w-1/2 p-4'>
-				<div className='bg-gray-800 bg-opacity-40 p-6 rounded-lg'>
+				<div className='bg-gray-800 bg-opacity-40 p-6 rounded-lg hover:bg-gray-700 transition-all duration-250'>
 					<img
 						className='h-40 rounded w-full object-cover object-center mb-6'
 						src={img}
